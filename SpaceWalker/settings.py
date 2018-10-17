6710d2725f57a3be470344ecffdff3fa434d2312
predawn1986 <44218108+predawn1986@users.noreply.github.com>
2018-10-17 23:46:44 +0800

    新增vueapp
    新增了vue的app

--- SpaceWalker/settings.py
+++ SpaceWalker/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'SpaceWalker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+        'DIRS': ['frontiuauto/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Add for Vue.js
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontiuauto/dist/static'),
+]
--- SpaceWalker/urls.py
+++ SpaceWalker/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from uiauto import views as uiauto_view
+from django.views.generic.base import TemplateView
+import uiauto
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
-    url(r'^index/', uiauto_view.index),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    # url(r'^index/', uiauto_view.index),
+    # url(r'^api/', include('uiauto.urls', namespace='api')),
 ]
